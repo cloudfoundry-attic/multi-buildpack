49f0bd2db3db920959d5e5395cc580e43e4db4d0
Sam Smith <sesmith177@gmail.com>
2016-09-02 14:38:42 -0400

    WIP; add support for downloading zip files
    Signed-off-by: Jacques Chester <jchester@pivotal.io>

--- lib/multi_buildpack_stager.rb
+++ lib/multi_buildpack_stager.rb
@@ -1,61 +1,97 @@
-
- require 'yaml'
- require 'uri'
-
+require 'fileutils'
+require 'yaml'
+require 'uri'
 
 class MultiBuildpackStager
 
-  attr_reader :build_dir, :cache_dir
+  attr_reader :build_dir, :cache_dir, :buildpack_downloads_dir
 
   def initialize(build_dir, cache_dir)
     @build_dir = build_dir
     @cache_dir = cache_dir
+    @buildpack_downloads_dir = File.join(build_dir, "multibuildpack-downloads-#{Random.rand(1000000)}")
+    FileUtils.mkdir_p(buildpack_downloads_dir)
   end
 
-  def get_buildpack_urls
+  def get_buildpacks
     multi_buildpack_file = File.join(build_dir, 'multibuildpack.yml')
-    YAML.load_file(multi_buildpack_file)['buildpacks']
+    buildpack_urls = YAML.load_file(multi_buildpack_file)['buildpacks']
+    download_buildpacks(buildpack_urls)
   end
 
-  def clone_buildpack(git_url)
-    puts "=====> Cloning buildpack #{git_url}..."
+  def download_buildpacks(buildpack_urls)
+    buildpacks = []
 
-    parsed_uri = URI(git_url)
+    Dir.chdir(buildpack_downloads_dir) do
+      buildpack_urls.each do |buildpack_url|
 
-    buildpack_name = parsed_uri.path.split('/').last
-    git_branch = parsed_uri.fragment
+        parsed_uri = URI(buildpack_url)
 
-    parsed_uri.fragment = nil
-    git_clone_url = parsed_uri.to_s
+        if is_zip_file(parsed_uri)
+          buildpacks.push(download_zip_file(parsed_uri))
+        else
+          buildpacks.push(clone_buildpack_repo(parsed_uri))
+        end
 
-    git_clone_command = "git clone #{git_clone_url}"
+      end
+    end
+
+    buildpacks
+  end
+
+  def is_zip_file(uri)
+    uri.path.split('/').last.end_with?('.zip')
+  end
+
+  def download_zip_file(uri)
+    puts "-----> Downloading buildpack #{uri.to_s}..."
+
+    output_file = uri.path.split('/').last
+    unzip_directory = output_file.chomp('.zip')
+
+    curl_command = "curl -L #{uri.to_s} -o #{output_file}"
+    curl_output = `#{curl_command}`
+    puts curl_output
+
+    puts "-----> Unzipping buildpack #{output_file} to #{unzip_directory}..."
+    unzip_command = "unzip #{output_file} -d #{unzip_directory}"
+    `#{unzip_command}`
+
+    unzip_directory
+  end
+
+  def clone_buildpack_repo(git_url)
+    puts "-----> Cloning buildpack #{git_url}..."
+
+    buildpack_name = git_url.path.split('/').last
+    git_branch = git_url.fragment
+    git_url.fragment = nil
+
+    git_clone_command = "git clone --depth 1 --recursive #{git_url.to_s} #{buildpack_name}"
     git_clone_command += " --branch #{git_branch}" unless git_branch.nil?
 
     puts git_clone_command
     puts `#{git_clone_command}`
 
-    if File.exist? "#{buildpack_name}/.gitmodules"
-      puts "=====> Detected git submodules. Initializing..."
-
-      Dir.chdir(buildpack_name) do
-        puts `git submodule update --init --recursive`
-      end
-    end
     buildpack_name
   end
 
-  def run_compile(buildpack_dir)
-    puts "=====> Running compile for buildpack #{buildpack_dir}..."
+  def run_compile(buildpack)
+    puts "-----> Running compile for buildpack #{buildpack}..."
+
+    buildpack_dir = File.join(buildpack_downloads_dir, buildpack)
 
     Dir.chdir(buildpack_dir) do
       compile_command = "bin/compile #{build_dir} #{cache_dir}"
-      puts `#{compile_command}`
+      compile_output = `#{compile_command}`
+      puts compile_output
     end
   end
 
-  def write_to_release_file(buildpack_dir)
-    puts "=====> Running release for buildpack #{buildpack_dir}..."
+  def write_to_release_file(buildpack)
+    puts "-----> Running release for buildpack #{buildpack}..."
 
+    buildpack_dir = File.join(buildpack_downloads_dir, buildpack)
     release_output = ''
 
     Dir.chdir(buildpack_dir) do
@@ -63,20 +99,20 @@ class MultiBuildpackStager
       release_output = `#{release_command}`
     end
 
-    release_output_file  = File.join(build_dir, 'last_pack_release.out')
+    release_output_file = File.join(build_dir, 'last_pack_release.out')
     File.write(release_output_file, release_output)
   end
 
   def run!
-    buildpacks_to_run = get_buildpack_urls
+    buildpacks = get_buildpacks
 
-    buildpack_names = buildpacks_to_run.map do |buildpack_url|
-      buildpack_dir = clone_buildpack(buildpack_url)
-      run_compile(buildpack_dir)
-      buildpack_dir
+    buildpacks.each do |buildpack|
+      run_compile(buildpack)
     end
 
-    write_to_release_file(buildpack_names.last)
+    write_to_release_file(buildpacks.last)
+
+    puts "-----> Removing buildpack downloads directory #{buildpack_downloads_dir}..."
+    FileUtils.rm_rf(buildpack_downloads_dir)
   end
 end
-
