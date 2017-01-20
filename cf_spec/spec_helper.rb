5b9c81e29bca08d38d3a7188ec2e72195072b30d
Dave Goddard <dave@goddard.id.au>
2017-01-20 10:49:54 -0500

    Allow stdout capture in specs

--- cf_spec/spec_helper.rb
+++ cf_spec/spec_helper.rb
@@ -12,3 +12,22 @@ RSpec.configure do |config|
   config.filter_run_excluding :cached => ENV['BUILDPACK_MODE'] == 'uncached'
   config.filter_run_excluding :uncached => ENV['BUILDPACK_MODE'] == 'cached'
 end
+
+module Kernel
+  @@io_semaphore = Mutex.new
+
+  [ :printf, :p, :print, :puts ].each do |io_write|
+    hidden_io_write = "__#{io_write}__"
+    alias_method hidden_io_write, io_write
+
+    define_method(io_write) do |*args|
+      @@io_semaphore.synchronize do
+        if log = Thread.current[:log]
+          log.__send__(io_write, *args)
+        else
+          self.__send__(hidden_io_write, *args)
+        end
+      end
+    end
+  end
+end
