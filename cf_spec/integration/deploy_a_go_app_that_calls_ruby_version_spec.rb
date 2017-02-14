e78c1326b5d4c4d9fe1648b33bdf80915dc96233
Sam Smith <sesmith177@gmail.com>
2017-02-14 11:56:12 -0500

    Multi buildpack prints its version
    [#139017911]
    
    Signed-off-by: Anna Thornton <athornton@pivotal.io>

--- cf_spec/integration/deploy_a_go_app_that_calls_ruby_version_spec.rb
+++ cf_spec/integration/deploy_a_go_app_that_calls_ruby_version_spec.rb
@@ -2,6 +2,8 @@ $: << 'cf_spec'
 require 'spec_helper'
 
 describe 'A go app that calls ruby version' do
+  let(:buildpack) { ENV.fetch('SHARED_HOST')=='true' ? 'multi_buildpack' : 'multi-test-buildpack' }
+  let(:app) { Machete.deploy_app(app_name, buildpack: buildpack) }
   let(:app_name) { 'go_app_that_calls_ruby_version' }
   let(:browser) { Machete::Browser.new(app) }
 
