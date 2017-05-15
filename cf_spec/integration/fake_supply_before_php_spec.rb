6bf8d7be6998209d04f591f783c20f3aebe38f40
Nikita Rathi <nrathi@pivotal.io>
2017-05-15 16:58:18 -0400

    Unskip php test
    [#142852347]
    
    Signed-off-by: Keaty Gross <kgross@pivotal.io>
    Signed-off-by: Nikita Rathi <nrathi@pivotal.io>

--- cf_spec/integration/fake_supply_before_php_spec.rb
+++ cf_spec/integration/fake_supply_before_php_spec.rb
@@ -2,7 +2,7 @@ $: << 'cf_spec'
 require 'yaml'
 require 'spec_helper'
 
-xdescribe 'running supply buildpacks before the php buildpack' do
+describe 'running supply buildpacks before the php buildpack' do
   let(:buildpack) { ENV.fetch('SHARED_HOST')=='true' ? 'multi_buildpack' : 'multi-test-buildpack' }
   let(:app) { Machete.deploy_app(app_name, buildpack: buildpack) }
   let(:browser) { Machete::Browser.new(app) }
