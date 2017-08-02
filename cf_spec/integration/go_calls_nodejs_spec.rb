f82057e97b8ce75da802db31c9c917524811111d
Forest Eckhardt <forest.eckhardt@gmail.com>
2017-08-02 10:06:50 -0400

    Fix spec description
    Signed-off-by: Sam Coward <scoward@pivotal.io>

--- cf_spec/integration/go_calls_nodejs_spec.rb
+++ cf_spec/integration/go_calls_nodejs_spec.rb
@@ -2,7 +2,7 @@ $: << 'cf_spec'
 require 'yaml'
 require 'spec_helper'
 
-describe 'running supply ruby buildpack before the go buildpack' do
+describe 'running supply nodejs buildpack before the go buildpack' do
   let(:buildpack) { ENV.fetch('SHARED_HOST')=='true' ? 'multi_buildpack' : 'multi-test-buildpack' }
   let(:app) { Machete.deploy_app(app_name, buildpack: buildpack) }
   let(:browser) { Machete::Browser.new(app) }
