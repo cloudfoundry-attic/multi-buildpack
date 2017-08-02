027412b002bf38bbba88f5db709bd80762a5fc6e
Dave Goddard <dave@goddard.id.au>
2017-08-02 16:38:21 -0400

    wip
    Signed-off-by: Forest Eckhardt <forest.eckhardt@gmail.com>

---
+++ scripts/integration.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -euo pipefail
+set -x
+
+export ROOT=$(dirname $(readlink -f ${BASH_SOURCE%/*}))
+if [ ! -f "$ROOT/.bin/ginkgo" ]; then
+  (cd "$ROOT/src/compile/vendor/github.com/onsi/ginkgo/ginkgo/" && go install)
+fi
+
+GINKGO_NODES=${GINKGO_NODES:-2}
+GINKGO_ATTEMPTS=${GINKGO_ATTEMPTS:-2}
+
+ginkgo -r --flakeAttempts=$GINKGO_ATTEMPTS -nodes $GINKGO_NODES
