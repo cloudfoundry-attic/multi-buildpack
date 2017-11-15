e7d9885424178bb9e6470f9d87e2a68e38a68e91
Jackson Feeny <jacksonfeeny@gmail.com>
2017-11-15 14:58:37 -0500

    Use cutlass test helper CopyCfHome [#150860504]
    Signed-off-by: Dave Goddard <dave@goddard.id.au>

--- src/compile/integration/integration_suite_test.go
+++ src/compile/integration/integration_suite_test.go
@@ -4,6 +4,8 @@ import (
 	"encoding/json"
 	"flag"
 	"fmt"
+	"os"
+	"strings"
 	"time"
 
 	"github.com/cloudfoundry/libbuildpack/cutlass"
@@ -51,12 +53,16 @@ var _ = SynchronizedBeforeSuite(func() []byte {
 	bpDir, err = cutlass.FindRoot()
 	Expect(err).NotTo(HaveOccurred())
 
+	Expect(cutlass.CopyCfHome()).To(Succeed())
 	cutlass.SeedRandom()
 	cutlass.DefaultStdoutStderr = GinkgoWriter
 })
 
 var _ = SynchronizedAfterSuite(func() {
 	// Run on all nodes
+	if strings.HasPrefix(os.Getenv("CF_HOME"), "/tmp") {
+		Expect(os.RemoveAll(os.Getenv("CF_HOME"))).To(Succeed())
+	}
 }, func() {
 	// Run once
 	Expect(cutlass.RemovePackagedBuildpack(packagedBuildpack)).To(Succeed())
